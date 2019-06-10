d88b34e2c251ccf87c6d9d164dfbfb6a2dead99d
Wolfgang Jeltsch <wolfgang@well-typed.com>
2019-06-10 23:30:16 +0300

    Add paper skeleton

---
+++ GNUmakefile
@@ -0,0 +1,7 @@
+build_pdfs = isabelle build -o document=pdf -d Paper
+
+all:
+	$(build_pdfs) Paper
+Paper:
+	$(build_pdfs) Paper
+.PHONY: all Paper
---
+++ Paper/Content.thy
@@ -0,0 +1,12 @@
+section \<open>Introduction\<close>
+
+text \<open>
+  This paper has been produced from documented Isabelle source
+  code\<^latex>\<open>~\cite{jeltsch:wflp-2019-source}\<close>, which has been checked with Isabelle2018.
+\<close>
+
+theory %invisible Content
+  imports Main
+begin
+
+end %invisible
