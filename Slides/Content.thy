79dff86370dd833fa20e30dc7e44b84e4a8ecf34
Wolfgang Jeltsch <wolfgang@well-typed.com>
2019-09-08 12:26:11 +0200

    Add subsection “Weak residuals”

--- Slides/Content.thy
+++ Slides/Content.thy
@@ -1,5 +1,5 @@
 theory %invisible Content
-  imports Chi_Calculus.Proper_Transition_System
+  imports Chi_Calculus.Proper_Weak_Transition_System
 begin
 
 section \<open>The \<open>\<natural>\<close>-calculus\<close>
@@ -360,5 +360,271 @@ text \<open>
 \<close>
 
 end %invisible
+
+subsection \<open>Weak residuals\<close>
+
+no_notation %invisible proper_transition ("_ \<rightarrow>\<^sub>\<sharp>_" [51, 51] 50)
+notation %invisible proper_transition ("_ \<rightarrow>_" [51, 51] 50)
+
+no_notation %invisible proper.weak_transition (infix "\<Rightarrow>\<^sub>\<sharp>" 50)
+notation %invisible proper.weak_transition ("_ \<Rightarrow>_" [51, 51] 50)
+
+abbreviation %invisible
+  silent_transition_closure :: "process \<Rightarrow> process \<Rightarrow> bool"
+  (infix "\<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>*" 50)
+where
+  "(\<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>*) \<equiv> (\<lambda>s t. s \<rightarrow>\<lparr>\<tau>\<rparr> t)\<^sup>*\<^sup>*"
+
+paragraph \<open>Weak bisimilarity\<close>
+
+lemma %invisible silent_weak_transition_def:
+  shows "p \<Rightarrow>\<lparr>\<tau>\<rparr> q \<longleftrightarrow> p \<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>* q"
+proof
+  assume "p \<Rightarrow>\<lparr>\<tau>\<rparr> q"
+  then show "p \<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>* q"
+  proof (induction p "\<lparr>\<tau>\<rparr> q" arbitrary: q)
+    case strong_transition
+    then show ?case
+      by (rule r_into_rtranclp)
+  next
+    case silent_transition
+    then show ?case
+      by (blast elim: proper_silent.cases)
+  next
+    case (composed_transition p _ q)
+    then obtain u where "p \<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>* u" and "u \<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>* q"
+      by (blast elim: proper_silent.cases proper_lift_cases)
+    then show ?case
+      by (rule rtranclp_trans)
+  qed
+next
+  assume "p \<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>* q"
+  then show "p \<Rightarrow>\<lparr>\<tau>\<rparr> q"
+  proof induction
+    case base
+    then show ?case
+      by (blast intro: proper_internal_is_silent proper.silent_transition)
+  next
+    case step
+    then show ?case
+      by (blast intro:
+        proper.strong_transition
+        proper_internal_is_silent
+        proper.composed_transition)
+  qed
+qed
+
+lemma %invisible observable_weak_transition_def:
+  fixes \<xi> :: proper_action
+  assumes "\<xi> \<noteq> \<tau>"
+  shows "p \<Rightarrow>\<lparr>\<xi>\<rparr> q \<longleftrightarrow> (\<exists>s t. p \<Rightarrow>\<lparr>\<tau>\<rparr> s \<and> s \<rightarrow>\<lparr>\<xi>\<rparr> t \<and> t \<Rightarrow>\<lparr>\<tau>\<rparr> q)"
+proof
+  assume "p \<Rightarrow>\<lparr>\<xi>\<rparr> q"
+  then show "\<exists>s t. p \<Rightarrow>\<lparr>\<tau>\<rparr> s \<and> s \<rightarrow>\<lparr>\<xi>\<rparr> t \<and> t \<Rightarrow>\<lparr>\<tau>\<rparr> q"
+  proof (induction p "\<lparr>\<xi>\<rparr> q" arbitrary: q)
+    case strong_transition
+    then show ?case
+      by (blast intro: proper_internal_is_silent proper.silent_transition)
+  next
+    case silent_transition
+    with \<open>\<xi> \<noteq> \<tau>\<close> show ?case
+      by (blast elim: proper_silent.cases)
+  next
+    case (composed_transition p _ q)
+    then consider
+      u and s and t where "p \<Rightarrow>\<lparr>\<tau>\<rparr> u" and "u \<Rightarrow>\<lparr>\<tau>\<rparr> s" and "s \<rightarrow>\<lparr>\<xi>\<rparr> t" and "t \<Rightarrow>\<lparr>\<tau>\<rparr> q" |
+      s and t and u where "p \<Rightarrow>\<lparr>\<tau>\<rparr> s" and "s \<rightarrow>\<lparr>\<xi>\<rparr> t" and "t \<Rightarrow>\<lparr>\<tau>\<rparr> u" and "u \<Rightarrow> \<lparr>\<tau>\<rparr> q"
+      by (blast elim: proper_silent.cases proper_lift_cases)
+    then show ?case
+      by cases (blast intro: proper_internal_is_silent proper.composed_transition)+
+  qed
+next
+  assume "\<exists>s t. p \<Rightarrow>\<lparr>\<tau>\<rparr> s \<and> s \<rightarrow>\<lparr>\<xi>\<rparr> t \<and> t \<Rightarrow>\<lparr>\<tau>\<rparr> q"
+  then show "p \<Rightarrow>\<lparr>\<xi>\<rparr> q"
+    by (fastforce intro:
+      proper_internal_is_silent
+      proper.strong_transition
+      proper.composed_transition)
+qed
+
+text \<open>
+  \<^item> \<^bold>\<open>Weak bisimilarity\<close> treats internal communication as \<^bold>\<open>silent\<close> and ignores it
+
+  \<^item> Elegant definition:
+
+      \<^item> Weak transitions:
+
+          \<^item> Silent:@{lemma [display, source]
+            "p \<Rightarrow>\<lparr>\<tau>\<rparr> q \<longleftrightarrow> p \<rightarrow>\<lparr>\<tau>\<rparr>\<^sup>*\<^sup>* q"
+            by (fact silent_weak_transition_def)}
+
+          \<^item> Observable:@{lemma [display, source]
+            "\<xi> \<noteq> \<tau> \<Longrightarrow>\<^latex>\<open>\\\<close>p \<Rightarrow>\<lparr>\<xi>\<rparr> q \<longleftrightarrow> (\<exists>s t. p \<Rightarrow>\<lparr>\<tau>\<rparr> s \<and> s \<rightarrow>\<lparr>\<xi>\<rparr> t \<and> t \<Rightarrow>\<lparr>\<tau>\<rparr> q)"
+            by (fact observable_weak_transition_def)}
+
+      \<^item> Weak bisimilarity: bisimilarity of the weak transition system
+
+  \<^item> The standard definition of weak transitions\\
+    is not appropriate for our residual-based approach,\\
+    because it refers to a \<^bold>\<open>dedicated silent label\<close>
+\<close>
+
+no_notation %invisible proper_transition ("_ \<rightarrow>_" [51, 51] 50)
+notation %invisible proper_transition ("_ \<rightarrow>\<^sub>\<sharp>_" [51, 51] 50)
+
+no_notation %invisible proper.weak_transition ("_ \<Rightarrow>_" [51, 51] 50)
+notation %invisible proper.weak_transition ("_ \<Rightarrow>\<^sub>\<sharp>_" [51, 51] 50)
+
+definition %invisible
+  basic_fuse :: "'p basic_residual basic_residual \<Rightarrow> 'p basic_residual \<Rightarrow> bool"
+where
+  "basic_fuse = basic_silent\<inverse>\<inverse> \<squnion> basic_lift basic_silent\<inverse>\<inverse>"
+
+(* A version of \<^theory_text>\<open>basic.silent_converse_naturality\<close> with a more general type *)
+lemma %invisible basic_silent_converse_naturality:
+  shows "basic_silent\<inverse>\<inverse> OO \<X> = basic_lift \<X> OO basic_silent\<inverse>\<inverse>"
+  by (blast
+    elim: basic_silent.cases basic_lift_cases
+    intro: basic_internal_is_silent basic_lift_intros)
+
+lemma %invisible basic_absorb_from_fuse:
+  shows "basic.absorb \<I> = basic_lift \<I> OO basic_fuse"
+  unfolding basic_fuse_def
+  by (simp add: basic_silent_converse_naturality basic_residual.rel_compp)
+
+paragraph \<open>Relations that identify silence\<close>
+
+text \<open>
+  \<^item> Definitions for the basic transition system:
+
+      \<^item> Silence introduction:@{lemma [display]
+        \<open>basic_silent p (\<lbrace>\<tau>\<rbrace> p)\<close>
+        by (fact basic_internal_is_silent)}
+
+      \<^item> Silence absorption:
+
+          \<^item> Silent--acting case:@{lemma [display, source]
+            "basic_fuse (\<lbrace>\<tau>\<rbrace>\<lbrace>\<alpha>\<rbrace> p) (\<lbrace>\<alpha>\<rbrace> p)"
+            by (unfold basic_fuse_def, blast intro: basic_internal_is_silent)}
+
+          \<^item> Silent--opening case:@{lemma [display, source]
+            "basic_fuse (\<lbrace>\<tau>\<rbrace>\<lbrace>\<nu> a\<rbrace> P a) (\<lbrace>\<nu> a\<rbrace> P a)"
+            by (unfold basic_fuse_def, blast intro: basic_internal_is_silent)}
+
+          \<^item> Acting--silent case:@{lemma [display, source]
+            "basic_fuse (\<lbrace>\<alpha>\<rbrace>\<lbrace>\<tau>\<rbrace> p) (\<lbrace>\<alpha>\<rbrace> p)"
+            by (unfold basic_fuse_def, blast intro: basic_internal_is_silent basic_lift_intros)}
+
+          \<^item> Opening--silent case:@{lemma [display, source]
+            "basic_fuse (\<lbrace>\<nu> a\<rbrace>\<lbrace>\<tau>\<rbrace> P a) (\<lbrace>\<nu> a\<rbrace> P a)"
+            by (unfold basic_fuse_def, blast intro: basic_internal_is_silent basic_lift_intros)}
+
+  \<^item> Definitions for the proper transition system: analogous
+\<close>
+
+no_notation %invisible basic_transition ("_ \<rightarrow>\<^sub>\<flat>_" [51, 51] 50)
+notation %invisible basic_transition (infix "\<rightarrow>\<^sub>\<flat>" 50)
+
+paragraph \<open>Weak transitions in the presence of scope opening\<close>
+
+text \<open>
+  \<^item> Definition for the basic transition system:
+
+      \<^item> Strong transitions:@{lemma [display]
+        \<open>p \<rightarrow>\<^sub>\<flat> c \<Longrightarrow> p \<Rightarrow>\<^sub>\<flat> c\<close>
+        by (fact basic.strong_transition)}
+
+      \<^item> Empty transitions:@{lemma [display]
+        \<open>basic_silent p c \<Longrightarrow> p \<Rightarrow>\<^sub>\<flat> c\<close>
+        by (fact basic.silent_transition)}
+
+      \<^item> Compound transitions:@{lemma [display]
+        \<open>\<lbrakk>p \<Rightarrow>\<^sub>\<flat> c; basic_lift (\<Rightarrow>\<^sub>\<flat>) c z; basic_fuse z d\<rbrakk> \<Longrightarrow> p \<Rightarrow>\<^sub>\<flat> d\<close>
+        by (rule basic.composed_transition, auto simp add: basic_absorb_from_fuse)}
+
+  \<^item> Definition for the proper transition system: identical\\
+    (apart from the use of different relations that identify silence)
+\<close>
+
+(*
+  From now on, we work with seemingly generic operations \<open>lift\<close>, \<open>silent\<close>, and \<open>fuse\<close> that are in
+  fact the concrete entities for the basic transition system. The reasons are as follows:
+
+    \<^item> There cannot be a generic \<open>fuse\<close> (because Isabelle does not support higher-kinded types).
+
+    \<^item> As a consequence of the former, there cannot be generic proofs involving \<open>fuse\<close> (we conduct
+      the proofs for the basic transition instead, which gives us at least \<^emph>\<open>some\<close> assurance.
+*)
+
+notation %invisible basic_lift ("lift")
+
+notation %invisible basic_silent ("silent")
+
+notation %invisible basic_fuse ("fuse")
+
+paragraph \<open>Silence-identifying relations axiomatically\<close>
+
+text \<open>
+  \<^item> Intuitions:
+
+      \<^item> Intuition behind \<^term>\<open>silent\<close>: a silent label is added
+
+      \<^item> Intuition behind \<^term>\<open>fuse\<close>:
+
+          \<^item> An outer silent label is removed
+
+          \<^item> An inner silent label is removed
+
+          \<^item> A nested residual with no silent labels is dropped
+
+  \<^item> Axioms that are in line with these intuitions:
+
+      \<^item> Left-neutrality:@{lemma [display]
+        \<open>silent OO fuse = (=)\<close>
+        by (
+          unfold basic_fuse_def,
+          blast elim: basic_silent.cases basic_lift_cases intro: basic_internal_is_silent
+        )}
+
+      \<^item> Right-neutrality:@{lemma [display]
+        \<open>lift silent OO fuse = (=)\<close>
+        by (fold basic_absorb_from_fuse, fact basic.right_neutrality)}
+
+      \<^item> Associativity:@{lemma [display]
+        \<open>fuse OO fuse = lift fuse OO fuse\<close>
+        by (
+          unfold basic_fuse_def,
+          simp add:
+            basic_residual.rel_compp [symmetric]
+            basic_silent_converse_naturality [symmetric]
+            sup_assoc
+            sup_commute
+            sup_left_commute
+        )}
+
+  \<^item> Residual structures with a notion of silence are \<^bold>\<open>monads\<close>\\
+    on the category of types and relations
+\<close>
+
+paragraph \<open>Strengths of the monadic approach to silence\<close>
+
+text \<open>
+  \<^item> More general than the traditional approach\\
+    (for example, multiple silent labels are possible)
+
+  \<^item> Expected properties of weak bisimilarity can be proved generically
+    (despite the additional generality)
+
+      \<^item> Weak bisimilarity is the same as ``mixed'' bisimilarity
+
+      \<^item> Strong bisimilarity is included in weak bisimilarity
+
+  \<^item> Enforces an elegant definition of weak transitions
+
+      \<^item> Distinction between silent and observable labels\\
+        is pushed into the definitions of concrete \<^term>\<open>silent\<close> and \<^term>\<open>fuse\<close> relations
+
+      \<^item> As a result, generic proofs tend to be simple
+\<close>
 
 end %invisible
