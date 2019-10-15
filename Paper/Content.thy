ee573a5a6004a04015b7055951488b3d30cb42db
Wolfgang Jeltsch <wolfgang@well-typed.com>
2019-10-16 00:06:01 +0300

    Add remaining headlines

--- Paper/Content.thy
+++ Paper/Content.thy
@@ -9,4 +9,22 @@ text \<open>
   code\<^latex>\<open>~\cite{jeltsch:wflp-2019-source}\<close>, which has been checked with Isabelle2018.
 \<close>
 
+section \<open>The \<open>\<natural>\<close>-calculus\<close>
+
+subsection \<open>Basics\<close>
+
+subsection \<open>Operational semantics\<close>
+
+subsection \<open>Behavioral equivalence\<close>
+
+section \<open>Residuals axiomatically\<close>
+
+subsection \<open>Residuals in general\<close>
+
+subsection \<open>Weak residuals\<close>
+
+subsection \<open>Normal weak residuals\<close>
+
+section \<open>Wrapping up\<close>
+
 end %invisible
